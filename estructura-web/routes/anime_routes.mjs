26eb256df3139dff54e855194c8bb5b971b52f74
LarisaM <alariko0@MacBook-Pro-de-Alariko0.local>
2026-01-04 14:54:06 +0100

    modal para edit de admin + css

--- estructura-web/routes/anime_routes.mjs
+++ estructura-web/routes/anime_routes.mjs
@@ -9,4 +9,22 @@ router.get("/catalog", taskController.listar)
 router.get("/detalles/:id", taskController.detalle)
 
 
+router.post("/update", async(req, res) => {
+    if(!req.session.user || req.session.user.rol !== "admin"){
+        return res.redirect("/accessDenied");
+    }
+
+    const { id, titulo, genero, episodios, descripcion } = req.body;
+    const consulta = `
+        UPDATE animes
+        SET titulo = ?, genero = ?, episodios = ?, descripcion = ?
+        WHERE id = ?
+    `;
+
+    await db.query(consulta, [titulo, genero, episodios, descripcion, id])
+
+    res.redirect("/catalog")
+})
+
+
 export default router
