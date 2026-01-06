5677054e2e27b6e05b7a29379f7844d37556f8e4
Alba <alba.rojas0305@gmail.com>
2026-01-06 22:55:01 +0100

    Cambios + Comprobaciones

--- estructura-web/controllers/firebase_controller.mjs
+++ estructura-web/controllers/firebase_controller.mjs
@@ -6,7 +6,8 @@ import {
   EmailAuthProvider,
   updatePassword,
   verifyBeforeUpdateEmail,
-  createUserWithEmailAndPassword, signOut
+  createUserWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
 
 class FirebaseController {
@@ -27,9 +28,13 @@ class FirebaseController {
 
       // Petición a la API
       const auth = getAuth();
-      let userCredential = await createUserWithEmailAndPassword(auth, req.body.email, req.body.password)
+      let userCredential = await createUserWithEmailAndPassword(
+        auth,
+        req.body.email,
+        req.body.password
+      );
 
-      if(userCredential){
+      if (userCredential) {
         await this.client.post("/insertUserData", {
           email: datosJson.email,
           nombre: datosJson.nombre,
@@ -42,91 +47,104 @@ class FirebaseController {
             uuid: userCredential.user.uid, // UID devuelto por la API
           },
           user: null,
-          errorL:null
+          errorL: null,
         });
-        
-      }else{
-        res.status(400).send("Error en el registro")
+      } else {
+        res.status(400).send("Error en el registro");
       }
     } catch (error) {
       console.error("Error al consumir la API:", error.message);
-      res.status(500).send("Error al registrarse");
+      res.render("completes/register", {
+        error: {
+          mensaje: "Error al registrarse (email en uso)",
+        },
+      });
     }
   };
 
   logIn = async (req, res) => {
     try {
-
       //cambiar estructura para que comprube si la cookie existe para utilizar las llamadas con la info del formulario o de la cookie existente
       const datosJson = {
         email: req.body.email,
         password: req.body.password,
       };
       const auth = getAuth();
-      let userCredential = await signInWithEmailAndPassword(auth,req.body.email, req.body.password)
-if (userCredential && userCredential.user) {
-            const userData = await this.client.get(
-            `/getUserData/${userCredential.user.uid}`
-          );
-          res.cookie("datosUsuario",
-            {
-              email: datosJson.email,
-              pass: datosJson.password,
-              uuid: userCredential.user.uid,
-              admin: userData.data[0].admin,
-              nombre: userData.data[0].nombre,
-            },
-            {
-              maxAge: 2 * 3600 * 100000000000,
-            }
-          );
+      let userCredential = await signInWithEmailAndPassword(
+        auth,
+        req.body.email,
+        req.body.password
+      );
+
+      if (userCredential && userCredential.user) {
+        const userData = await this.client.get(
+          `/getUserData/${userCredential.user.uid}`
+        );
+        res.cookie(
+          "datosUsuario",
+          {
+            email: datosJson.email,
+            pass: datosJson.password,
+            uuid: userCredential.user.uid,
+            admin: userData.data[0].admin,
+            nombre: userData.data[0].nombre,
+          },
+          {
+            maxAge: 2 * 3600 * 100000000000,
+          }
+        );
 
         // tiene como atributos rankingAnimeFav.count y rankingAnimeFav.titulo
-        let rankingAnimeFav = await this.client.post("/getAnimeFollowStat")
+        let rankingAnimeFav = await this.client.post("/getAnimeFollowStat");
 
         // tiene como atributos rankingUserFav.count y rankingUserFav.nombre
-        let rankingUserFav = await this.client.post("/getUserFollowStat")
+        let rankingUserFav = await this.client.post("/getUserFollowStat");
 
-        if(!rankingAnimeFav || !rankingAnimeFav.data){
-          console.log("No se pudo obtener el ranking de animes favoritos")
+        if (!rankingAnimeFav || !rankingAnimeFav.data) {
+          console.log("No se pudo obtener el ranking de animes favoritos");
         }
 
-        if(!rankingUserFav || !rankingUserFav.data){
-          console.log("No se pudo obtener el ranking de usuarios con mas favoritos")
+        if (!rankingUserFav || !rankingUserFav.data) {
+          console.log(
+            "No se pudo obtener el ranking de usuarios con mas favoritos"
+          );
         }
 
-        console.log("rankingAnimeFav:", rankingAnimeFav.data)
-console.log("rankingUserFav:", rankingUserFav.data)
+        console.log("rankingAnimeFav:", rankingAnimeFav.data);
+        console.log("rankingUserFav:", rankingUserFav.data);
 
         res.render("completes/index", {
           user: userData.data[0],
           rankingAnimeFav: rankingAnimeFav.data,
           rankingUserFav: rankingUserFav.data,
         });
-
-        }else{
-          res.status(400).send("Error: Email")
-        }
+      } else {
+        res.status(400).send("Error: Email");
+      }
     } catch (error) {
       console.error("Error al consumir la API:", error.message);
+
       res.render("completes/logIn", {
-        errorL : {mensaje : "Error al iniciar sesión"},
-        log: { email: req.body.email, pass: req.body.password } 
-      })
+        errorL: { mensaje: "Error al iniciar sesión" },
+        log: { email: req.body.email, pass: req.body.password },
+      });
     }
   };
 
   //Añadi: Limpiar cookie para que la nav se resetee (no estaba puesto) y redirige al login para que se recargue
   signOutUser = async (req, res) => {
     try {
-      const auth = getAuth()
-      let userCredential = await signOut(auth)
-      res.clearCookie("datosUsuario")
+      const auth = getAuth();
+      let userCredential = await signOut(auth);
+      res.clearCookie("datosUsuario");
       res.redirect("/login");
-      
     } catch (error) {
       console.error("Error al consumir la API:", error.message);
-      res.status(500).send("Error al buscar todas las tareas");
+      res.render("completes/logOut", {
+        error: {
+          mensaje: "Error al cerrar sesión",
+        },
+      });
     }
   };
 
@@ -139,7 +157,6 @@ console.log("rankingUserFav:", rankingUserFav.data)
       if (!userData) {
         return res.json({ error: "No estas logueado" });
       }
-
 
       const auth = getAuth();
 
@@ -154,8 +171,6 @@ console.log("rankingUserFav:", rankingUserFav.data)
 
       await updatePassword(user, req.body.newPass);
 
-
-
       // Actualizar cookie con la nueva contraseña
       res.cookie("datosUsuario", {
         email: userData.email,
@@ -167,12 +182,13 @@ console.log("rankingUserFav:", rankingUserFav.data)
 
       res.json({ mensaje: "Contraseña actualizada" });
     } catch (error) {
+      if(error.code === "auth/invalid-credential"){
+            return res.status(400).json({ error: "Contraseña incorrecta" });
+      }
       console.error("Error al consumir la API:", error.message);
-      res.status(500).send("Error al cambiar la contraseña");
     }
   };
 
-  //te deberia devolver a la pagina de datos del usuario; esto se deberia aplicar para todos los updates y delete
   //Comprueba la contraseña a través de la verificación del login, si se le pasa bien la contraseña
   //Actualiza el email antiguo al nuevo con updateEmail y actualiza la cookie.
   updateEmail = async (req, res) => {
@@ -182,7 +198,6 @@ console.log("rankingUserFav:", rankingUserFav.data)
 
       const auth = getAuth();
 
-      // Login para sesión válida
       const userCredential = await signInWithEmailAndPassword(
         auth,
         userData.email,
@@ -190,7 +205,6 @@ console.log("rankingUserFav:", rankingUserFav.data)
       );
 
       const user = userCredential.user;
-
 
       const credential = EmailAuthProvider.credential(
         user.email,
@@ -201,25 +215,20 @@ console.log("rankingUserFav:", rankingUserFav.data)
 
       await verifyBeforeUpdateEmail(user, req.body.newEmail);
 
-      let datos = await this.client.post("/updateUserEmail",
-        {
-          id:userData.uuid,
-          email:req.body.newEmail
-        }
-      )
-
-      res.cookie("datosUsuario", {
-        email: req.body.newEmail,
-        pass: userData.pass,
-        uuid: userData.uuid,
-        admin: userData.admin,
-        nombre: userData.nombre,
+      return res.json({
+        mensaje:
+          "Revisa tu bandeja de entrada o spam que te hemos enviado en tu nuevo correo electrónico para confirmar el cambio de email",
       });
-
-      res.json({ mensaje: "Email actualizado correctamente" });
     } catch (error) {
-      console.error("Error al consumir la API:", error.message);
-      res.status(500).send("Error al cambiar el email");
+      if (
+        error.code === "auth/invalid-credential" ||
+        error.code === "auth/invalid-login-credentials" ||
+        error.code === "auth/wrong-password"
+      ) {
+        return res.status(400).json({ error: "Contraseña incorrecta" });
+      }
+
+      return res.status(400).json({ error: "Error al cambiar el email" });
     }
   };
 
@@ -227,23 +236,27 @@ console.log("rankingUserFav:", rankingUserFav.data)
     try {
       const userData = req.cookies["datosUsuario"];
       if (!userData) return res.status(401).json({ error: "No logueado" });
-      let datos = await this.client.post("/updateUserName",{
-        id:userData.uuid,
-        nombre:req.body.newName
-      })
+      let datos = await this.client.post("/updateUserName", {
+        id: userData.uuid,
+        nombre: req.body.newName,
+      });
 
       res.cookie("datosUsuario", {
         email: userData.email,
         pass: userData.pass,
         uuid: userData.uuid,
         admin: userData.admin,
-        nombre: req.body.newName
+        nombre: req.body.newName,
       });
 
-      res.json({ mensaje: "Email actualizado correctamente" });
+      res.json({ mensaje: "Nombre actualizado correctamente" });
     } catch (error) {
       console.error("Error al consumir la API:", error.message);
-      res.status(500).send("Error al cambiar el email");
+      res.render("completes/changeName", {
+        error: {
+          mensaje: "Error al cambiar el nombre",
+        },
+      });
     }
   };
 
@@ -255,15 +268,20 @@ console.log("rankingUserFav:", rankingUserFav.data)
         return res.redirect("/logIn");
       }
 
-      const datos = await this.client.get(`/getAllFav/${req.cookies["datosUsuario"].uuid}`)
+      const datos = await this.client.get(
+        `/getAllFav/${req.cookies["datosUsuario"].uuid}`
+      );
       res.render("completes/profile", {
         favData: datos.data,
         user: userData,
       });
-      
     } catch (error) {
       console.error("Error:", error.message);
-      res.status(500).send("Error al cargar el perfil");
+      res.render("completes/profile", {
+        error: {
+          mensaje: "Error al cargar el perfil",
+        },
+      });
     }
   };
 }
